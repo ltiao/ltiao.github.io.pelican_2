0607ce5208e29eed71471d1a7d32c32c5ed3c8f0
Louis Tiao <louistiao@gmail.com>
2015-02-04 02:11:33 +0800

    theming

--- content/pages/about.rst
+++ content/pages/about.rst
@@ -2,4 +2,15 @@
 About
 =====
 
+.. raw:: html 
+
+   <blockquote class="blockquote-reverse">
+     <p>I may not be there yet, but I'm closer than I was yesterday.</p>
+     <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+   </blockquote>
+
 Hi, I'm Louis.
+
+The best way to reach me is at `l..@me.com`_.
+
+.. _l..@me.com: http://www.google.com/recaptcha/mailhide/d?k=01ZGES3iSWmUwr35sEbB8-VA==&c=PeD7vZlw1_DRu8fsayKDuVdVl_rtu18xfsGBgyvNXwc=
--- pelicanconf.py
+++ pelicanconf.py
@@ -28,15 +28,28 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('Twitter', '#'),
-		  ('Linkedin', '#'),
-          ('Facebook', '#'),
-          ('Instagram', '#'),
-          ('Github', '#'),)
+SOCIAL = (
+	('Github', 'https://github.com/ltiao', 'github'),
+	('Linkedin', 'https://www.linkedin.com/in/ltiao', 'linkedin'),
+	('Twitter', 'https://twitter.com/louistiao', 'twitter'),
+	('Facebook', 'https://www.facebook.com/louis.tiao', 'facebook'),
+	('Email', 'http://www.google.com/recaptcha/mailhide/d?k=01ZGES3iSWmUwr35sEbB8-VA==&c=PeD7vZlw1_DRu8fsayKDuVdVl_rtu18xfsGBgyvNXwc=', 'envelope')
+)
 
-DEFAULT_PAGINATION = 5
+# SOCIAL = [(a, b) for a, b, _ in SOCIAL]
+
+DEFAULT_PAGINATION = 1
+
+DISQUS_SITENAME = 'ltiao'
 
 GOOGLE_ANALYTICS = 'UA-43722566-1'
+
+GITHUB_URL = 'https://github.com/ltiao'
+
+# MENUITEMS = [
+# 	('Test', 'http://www.cse.unsw.edu.au/~ctia193'),
+# 	('Something', 'http://www.reddit.com'),
+# ]
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -47,5 +60,5 @@ PLUGIN_PATHS = ['plugins']
 THEME = 'themes/tiao-pelican-bootstrap3'
 BOOTSTRAP_USE_CDN = True
 BOOTSTRAP_THEME = 'yeti'
-
-# BOOTSTRAP_FLAT = True
+# BOOTSTRAP_USE_PAGER = True
+BOOTSTRAP_FLAT = True
